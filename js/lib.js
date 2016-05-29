7a3b30e25dec98cefe7ce4dd269614f35dc35ce6
= <=>
2016-05-29 03:13:03 +0200

    Added lib with FizzBuzz implementation

--- js/lib.js
+++ js/lib.js
@@ -1,3 +1,13 @@
-console.log("Bonjour en Javascript!");
-var test = document.getElementById("paragraph");
-console.log(test);
+for (var i = 1; i <=100; i++){
+    var output = "";
+    if (getSpecialMessage(i,3)) output = output + "Fizz";
+    if (getSpecialMessage(i,5)) output = output + "Buzz";
+    if (output === "")
+        console.log(i);
+    else 
+        console.log(output);
+}
+
+function getSpecialMessage(iterator, moduloValue){
+    if (iterator%moduloValue === 0) return true;
+}
